0564b8c08769be7ed12f35a257bff2198e41dff9
Ashwini <ashjd29@gmail.com>
2016-03-30 17:51:50 -0700

    write a main function  which gives the array of elements with the given className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+var getElementsByClassName = function(className){
+	var body = document.body;
+	var result = [];
+	if (body != null) {
+		if (body.classList.contains(className)) {
+			result.push(body);
+		}
+		var childNodes = body.childNodes;
+		var result = result.concat(getElementsArray(childNodes, className));
+	}
+	return result;
 };
+
+};
+
